c3450d18ecc1a629db38d47b9383bbb777bdf792
bhyun <nkmine100@gmail.com>
2021-06-09 16:41:52 +0900

    Revert "재귀, 구현"
    This reverts commit 7ea8912a92238c6a2fa0c1ec304ab09a3db4eb5f.

--- Programmers/Kakao_괄호 변환.py
+++ Programmers/Kakao_괄호 변환.py
@@ -1,39 +1,44 @@
-def split(word):
+def check(p):
     cnt = 0
-    for i, w in enumerate(word):
-        if w == "(":
-            cnt += 1
-        else:
-            cnt -= 1
-        if cnt == 0:
-            return word[:i + 1], word[i + 1:]
-
-
-def isright(word):
-    cnt = 0
-    for w in word:
-        if w == "(":
+    for i in p:
+        if i == "(":
             cnt += 1
         else:
             cnt -= 1
             if cnt < 0:
-                return False
-    return True
+                return 0 # 균형잡힌 문자열
+    return 1 # 올바른 문자열
 
+def divide(p):
+    cnt = 0
+    u = ""
+    for i, s in enumerate(p):
+        if s == "(":
+            u += s
+            cnt += 1
+        if s == ")":
+            cnt -= 1
+            u += s
+        if cnt == 0:
+            break
+    return u, p[i+1:] # 균형, 올바른
 
 def solution(p):
     if p == "":
         return ""
-    u, v = split(p)
-    if isright(u):  # 올바른 문자열
-        return u + solution(v)
-    else:  # 균형잡힌 문자열
-        string = "("
-        string += solution(v)
-        string += ")"
-        for s in u[1:-1]:
-            if s == "(":
-                string += ")"
+
+    u, v = divide(p)
+    if check(u): # u가 올바른 문자열이면
+        ans = u
+        ans += solution(v)
+        return ans
+    else: # u가 균형잡힌 문자열
+        ans = "("
+        ans += solution(v)
+        ans += ")"
+        for i in u[1:-1]:
+            if i == '(':
+                ans += ')'
             else:
-                string += "("
-        return string
+                ans += '('
+        return ans
