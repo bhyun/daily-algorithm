c6cfd2187bf592d6f425dba34d45a99812035e4f
bhyun <nkmine100@gmail.com>
2022-02-07 12:10:42 +0900

    Modify Programmers_경주로 건설.py

--- 2022/2022.02/Programmers_경주로 건설.py
+++ 2022/2022.02/Programmers_경주로 건설.py
@@ -2,54 +2,38 @@ from collections import deque
 import sys
 
 def bfs(x, y, board):
+    n = len(board)
+    q = deque()
+    cost = [[[sys.maxsize] * 4 for _ in range(n)] for _ in range(n)]
+    for i in range(4):
+        q.append((0, 0, i, 0))
     answer = sys.maxsize
 
-    n = len(board)
-    cost = [[sys.maxsize] * n for _ in range(n)]
-    cost[x][y] = 0
-
-    q = deque()
-    q.append((x, y, -1, 0))
-
-    dx = [1, 0, -1, 0]
-    dy = [0, 1, 0, -1]
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, 1, -1]
 
     while q:
         x, y, d, dist = q.popleft()
 
-        if cost[x][y] < dist:
+        if cost[x][y][d] < dist:
             continue
+
         if x == n - 1 and y == n - 1:
             answer = min(answer, dist)
-            continue
-        if dist > answer:
             continue
 
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
             if 0 <= nx < n and 0 <= ny < n and board[nx][ny] == 0:
-                if i % 2 == 0:
-                    nd = 0
-                else:
-                    nd = 1
-
-                if d == -1 and dist + 100 <= cost[nx][ny]:
-                    cost[nx][ny] = dist + 100
-                    q.append((nx, ny, nd, dist + 100))
-                elif d != nd and dist + 600 <= cost[nx][ny]:
-                    cost[nx][ny] = dist + 600
-                    q.append((nx, ny, nd, dist + 600))
-                elif d == nd and dist + 100 <= cost[nx][ny]:
-                    cost[nx][ny] = dist + 100
-                    q.append((nx, ny, nd, dist + 100))
+                if d == i and cost[nx][ny][i] > dist + 100:
+                    cost[nx][ny][i] = dist + 100
+                    q.append((nx, ny, i, dist + 100))
+                elif d != i and cost[nx][ny][i] > dist + 600:
+                    cost[nx][ny][i] = dist + 600
+                    q.append((nx, ny, i, dist + 600))
     return answer
-
 
 def solution(board):
     answer = bfs(0, 0, board)
     return answer
-
-# 마지막 테스트 통과못함
-# 내일 재도전
-print(solution(	[[0, 0, 0, 0, 0, 0, 0, 0], [1, 0, 1, 1, 1, 1, 1, 0], [1, 0, 0, 1, 0, 0, 0, 0], [1, 1, 0, 0, 0, 1, 1, 1], [1, 1, 1, 1, 0, 0, 0, 0], [1, 1, 1, 1, 1, 1, 1, 0], [1, 1, 1, 1, 1, 1, 1, 0], [1, 1, 1, 1, 1, 1, 1, 0]]))
