e1bb3aa303da2e61eb74495c0f468d73b0069aa2
bhyun <nkmine100@gmail.com>
2022-03-31 13:01:36 +0900

    Modify Programmers_디스크 컨트롤러.py

--- 2022/2022.02/Programmers_디스크 컨트롤러.py
+++ 2022/2022.02/Programmers_디스크 컨트롤러.py
@@ -1,27 +1,36 @@
 import heapq
 
 def solution(jobs):
+    n = len(jobs)
+
+    jobs.sort(key=lambda x: (x[0], x[1]))
+
+    seconds = 0
     answer = 0
+    visited = [False] * n
 
-    jobs.sort(key=lambda x: x[0])
-    q = []
-    time = 0
-    completed = 0
-    visited = [False] * len(jobs)
-    while completed < len(jobs):
-        for i, job in enumerate(jobs):
-            if not visited[i] and job[0] <= time:
-                visited[i] = True
-                heapq.heappush(q, (job[1], job[0]))
+    while True:
+        controller = []
 
-        if q:
-            duration, start = heapq.heappop(q)
-            time += duration
-            answer += (time - start)
-            completed += 1
+        for i in range(n):
+            start, duration = jobs[i][0], jobs[i][1]
+            if not visited[i] and start <= seconds:
+                heapq.heappush(controller, (seconds + duration, i))
+
+        # 하드디스크가 작업을 수행하고 있지 않을 때에는 먼저 요청이 들어온 작업부터 처리
+        if not controller:
+            for i in range(n):
+                if not visited[i]:
+                    start, duration = jobs[i][0], jobs[i][1]
+                    heapq.heappush(controller, (start + duration, i))
+                    break
+
+        if controller:
+            time, cur = heapq.heappop(controller)
+            visited[cur] = True
+            answer += time - jobs[cur][0]
+            seconds = time
         else:
-            time += 1
+            break
 
-    return answer // len(jobs)
-
-print(solution([[0, 3], [1, 9], [2, 6]]	))
+    return answer // n
